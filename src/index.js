792ad04b45c5367f6f5544868bb25bae98a499e3
AlexeyTeterin <teterin.alexey@gmail.com>
2020-02-20 11:19:31 +0500

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let numAsString = n.toString();
+  let reversedString = '';
+    for (let i = 0; i < numAsString.length; i++) {
+        reversedString += numAsString.charAt(numAsString.length-i-1);
+    }
+    return parseInt(reversedString);
 }
